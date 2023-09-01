a92a90ad70a68ed7065a89b17407596f751cd1d2
ToanNguyen <macorz@vatek.asia>
2023-09-01 22:35:24 +0700

    create

--- src/main/java/com/example/datn/DatnApplication.java
+++ src/main/java/com/example/datn/DatnApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DatnApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DatnApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DatnApplication.class, args);
+	}
 
 }
